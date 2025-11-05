f219493faad9f9393980ac5f0376fb828c000d05
Nat <natup1977@gmail.com>
2025-11-05 14:01:59 +0200

    Fix task 4: use alert and object output

--- js/task-4.js
+++ js/task-4.js
@@ -5,13 +5,16 @@ registerForm.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
-  
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
   if (email === "" || password === "") {
-    return console.log("All form fields must be filled in");
+    alert("All form fields must be filled in");
+    return;
   }
-
-  console.log(`Email: ${email}, Password: ${password}`);
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
   form.reset();
 }
